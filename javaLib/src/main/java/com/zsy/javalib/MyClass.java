656fa00b648107ab14ed627dccf68398fdc389a1
maozs <mzs@zjjzfy.com>
2019-06-05 14:46:20 +0800

    add lib

---
+++ javaLib/src/main/java/com/zsy/javalib/MyClass.java
@@ -0,0 +1,8 @@
+package com.zsy.javalib;
+
+public class MyClass {
+
+    public static void test() {
+        System.out.println("javaLib");
+    }
+}
---
+++ lib2/src/main/java/com/zsy/lib2/LibUtils.java
@@ -0,0 +1,10 @@
+package com.zsy.lib2;
+
+import android.util.Log;
+
+public class LibUtils {
+
+    public static void test() {
+        Log.i("LgTest", "Test");
+    }
+}
